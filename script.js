52dddcbbbb42b72dfea302eaba9ea5f47eea4c60
Janette <janette.castillo91@yahoo.com>
2021-11-08 00:16:18 -0600

    added history tab to  page

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 var inputField = document.getElementById('search');
 var button = document.getElementById('searchCity');
-var searchHistoryList = document.getElementById('search-history-list');
+var historyDisplay = document.getElementById('search-history-list');
 var currentTemp = document.getElementById('currentCityTemp');
 var forecastDisplay = document.getElementById('forecast');
 var pastSearchesEl = localStorage.getItem('history') || '[]';
+var card = document.getElementById('#card-body')
 var pastSearches = JSON.parse(pastSearchesEl);
+var fiveDayEl = document.getElementById('fiveDay');
 button.addEventListener("click", fetchData)
 button.addEventListener("click", fetchForecastData)
 button.addEventListener("click", historyData)
+button.addEventListener("click", resetPage)
 
-function fetchData(event){
-    event.preventDefault();
+
+//weather data
+function fetchData(){
+    event.preventDefault()
+  
         var cityName = inputField.value
         var apiKey = 'fd531081518e808eb0375251a19ac935'
         var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey
         
-     
+        //fetch request for weather data
         fetch(requestUrl)
             .then(function (response) {
                 return response.json();
@@ -23,78 +29,68 @@ function fetchData(event){
             .then(function (weatherData) {
                 console.log("weather", weatherData);
 
+            //lat and lon local storage
             var lat = weatherData.coord.lat
             console.log("lat", lat)
             localStorage.setItem("lat", JSON.stringify(lat))
+
             var lon = weatherData.coord.lon
             console.log("lon", lon)
             localStorage.setItem("lon", JSON.stringify(lon))
+
+            //city name local storage
+            console.log("cityName", cityName)
+            localStorage.setItem("cityName", JSON.stringify(cityName))
         
+            //city name and date
             var time = moment().format("MM/DD/YYYY");
             var timeEl= time
             var cityEl = document.createElement('h3');
             cityEl.textContent = weatherData.name + "(" + timeEl + ")";
-            var currentTemp =document.body.children[1].children[1].children[0].children[0]
+            var currentTemp =document.body.children[1].children[1].children[0]
             currentTemp.append(cityEl);
 
-            var cityName = weatherData.name
-            console.log("cityName", cityName)
-            localStorage.setItem("cityName", JSON.stringify(cityName))
-
+            //icon
             var icon = document.createElement('img')
             icon.src= 'https://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png'
             var currentTemp = document.body.children[1].children[1].children[0] 
             cityEl.appendChild(icon)
 
+            //current city temp
             var cityTemp = document.createElement('p');
             cityTemp.textContent = " Temp: " + ((weatherData.main.temp_max - 273) * 1.8 + 32).toFixed(2) +  "\u2109";
             currentTemp.append(cityTemp);
-
+            
+            //wind
             var cityWind = document.createElement('p');
             cityWind.textContent = " Wind: " + weatherData.wind.speed + " MPH ";
             currentTemp.append(cityWind);
 
+            //humidity
             var cityHumidity = document.createElement('p');
             cityHumidity.textContent = " Humidity: " + weatherData.main.humidity + " % ";
             currentTemp.append(cityHumidity);
 
+            //clears input field
             inputField.value = "";
-            resetContent();
-          
+            resetPage();
             })
         }
-
-        // function saveCity(cityName) {
-        //     if (inputField.value === '' || pastSearches.includes(cityName)) return;
-        //     makeNewBtn(cityName)
-        //     inputField.value = '';
-        
-        //     pastSearches.push(cityName);
-        //     localStorage.setItem('history', JSON.stringify(pastSearches));
-        // }
-function historyData(){
-           
-    var cityName2 = JSON.parse(localStorage.getItem("cityName"))
-    console.log("cityName2", cityName2)
-    var newBtn = document.createElement('button')
-    newBtn.setAttribute('class', 'history-btn')
-    newBtn.textContent = cityName2;
-    searchHistoryList .append(newBtn);
-    newBtn.addEventListener('click', function (event) {
-    event.preventDefault;
-    fetchData(newBtn.textContent);
-            })
-        }
-
-    
+        function resetPage() {
+            inputField.textContent = '';
+            // removeForecastEls();
+        } 
+ //forecast data   
 function fetchForecastData() {
 
+            //lat and lon grabbing local storage from previous function
             var lat2 = JSON.parse(localStorage.getItem("lat"))
             console.log("lat2", lat2)
 
             var lon2 = JSON.parse(localStorage.getItem("lon"))
             console.log("lon2", lon2)
 
+            //UV Index api fetch
             var apiKey = 'fd531081518e808eb0375251a19ac935'
             var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat2 + "&lon=" + lon2 + "&exclude=hourly&units=imperial&appid="  + apiKey
             console.log(uvURL)
@@ -105,6 +101,7 @@ function fetchForecastData() {
             .then(function(uvData){
                 console.log(uvData)
 
+                //appending UV Index to the currentTemp container
                 var cityUv = document.createElement('p');
                 var uvIndex = uvData.current.uvi
                 cityUv.textContent = " UV Index: " + uvIndex;
@@ -161,7 +158,36 @@ function fetchForecastData() {
           
             })
         }
+        // // Saves city name in history
+        // function saveCity(cityName) {
+        //     if (inputField.value === '' || pastSearches.includes(cityName)) return;
+        //     historyData(cityName)
+        //     inputField.value = '';
+
+        //     pastSearches.push(cityName);
+        //     localStorage.setItem('history', JSON.stringify(pastSearches));
+        // }
+
+        function historyData(){
+           
+            var cityName2 = JSON.parse(localStorage.getItem("cityName"))
+            console.log("cityName2", cityName2)
         
+            var historyBtn = document.createElement('button')
+            historyBtn.setAttribute('class', 'history-btn')
+            historyBtn.textContent = cityName2;
+            historyDisplay.append(historyBtn);
+                }
+
+        // Populates history section with city names saved in local storage
+        function populateHistory() {
+            for (var i = 0; i < pastSearches.length; i++) {
+                historyData(pastSearches[i]);
+            }
+        }
         
 
-        button.addEventListener("click", resetContent)
+        button.addEventListener("click", resetPage)
+        button.addEventListener("click", saveCity)
+        historyBtn.addEventListener('click', fetchData())
+       
